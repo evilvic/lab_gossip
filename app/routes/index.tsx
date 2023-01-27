f1b0a992dab4470cc734d941b652f958a8627dda
Víctor Peña Romero <vic.pero@icloud.com>
2023-01-27 07:45:55 -0600

    generate type definitions

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,14 @@
 import { useLoaderData } from "@remix-run/react"
 import supabase from "utils/supabase"
 
-export const loader = async () => {
+import type { LoaderArgs } from "@remix-run/node"
+
+export const loader = async ({}: LoaderArgs) => {
   const { data } = await supabase.from("messages").select()
-  return { data }
+  return { messages: data ?? [] }
 }
 
 export default function Index() {
-  const { data } = useLoaderData()
-  return <pre>{JSON.stringify(data, null, 2)}</pre>
+  const { messages } = useLoaderData<typeof loader>()
+  return <pre>{JSON.stringify(messages, null, 2)}</pre>
 }
