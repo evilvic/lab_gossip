cb9134c483b7819d2c3d7500a3eee81b3e4da252
Víctor Peña Romero <vic.pero@icloud.com>
2023-02-01 07:47:17 -0600

    add send new message feature

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,23 @@
-import { useLoaderData } from "@remix-run/react"
+import { Form, useLoaderData } from "@remix-run/react"
 import createServerSupabase from "utils/supabase.server"
 
-import { json, LoaderArgs } from "@remix-run/node"
+import { ActionArgs, json, LoaderArgs } from "@remix-run/node"
 import Login from "components/login"
+
+export const action = async ({ request }: ActionArgs) => {
+  const response = new Response()
+  const supabase = createServerSupabase({ request, response })
+
+  const { message } = Object.fromEntries(await request.formData())
+  const { error } = await supabase.from("messages").insert({ content: String(message) })
+
+  if (error) {
+    console.log(error);
+    
+  }
+
+  return json(null, { headers: response.headers })
+}
 
 export const loader = async ({ request }: LoaderArgs) => {
   const response = new Response()
@@ -17,6 +32,10 @@ export default function Index() {
     <>
       <Login/>
       <pre>{JSON.stringify(messages, null, 2)}</pre>
+      <Form method="post">
+        <input type="text" name="message" />
+        <button type="submit">Send</button>
+      </Form>
     </>
   )
   
